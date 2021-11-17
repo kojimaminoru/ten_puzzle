22c8295525a91f0747810c35c6174f929943c936
poisonedapple <s18515@salesio-sp.ac.jp>
2021-11-17 22:39:55 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string.h>
+using namespace std;
+#define NUMS 4
+#define TARGET 10
+
+int main(int argc, const char * argv[]) {
+    int a;
+    vector<int> num(NUMS);
+    cin >> a;
+    cout << a << endl;
+    return 0;
+}
