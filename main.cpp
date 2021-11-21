fcf149a89909316f9c0a7ba1f5b42062591591b3
poisonedapple <s18515@salesio-sp.ac.jp>
2021-11-21 23:33:07 +0900

    commit 21/11/21

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 #include <vector>
 #include <string.h>
 #include <stack>
+#include <math.h>
 using namespace std;
 #define rep(i, b) for(int i = 0; i < int(b); i++)
 #define NUMS 4
 #define TARGET 10
+#define delta 1e-14
 
 bool isOpe(char c){
     if(c == '+' || c == '-' || c == '*' || c == '/') return 1;
@@ -22,7 +24,33 @@ double ReversePolishNotation(string s){
                 list.pop();
                 b = list.top();
                 list.pop();
-                list.push(a + b);
+                list.push(b + a);
+            }
+            else if(s[0] == '-'){
+                a = list.top();
+                list.pop();
+                b = list.top();
+                list.pop();
+                list.push(b - a);
+            }
+            else if(s[0] == '*'){
+                a = list.top();
+                list.pop();
+                b = list.top();
+                list.pop();
+                list.push(b * a);
+            }
+            else if(s[0] == '/'){
+                a = list.top();
+                list.pop();
+                b = list.top();
+                list.pop();
+                if(fabs(a) < delta){
+                    cout << "division by zero" << endl;
+                    s = "no";
+                    break;
+                }
+                list.push(b / a);
             }
             s = s.substr(1);
         }
@@ -31,21 +59,22 @@ double ReversePolishNotation(string s){
             s = s.substr(1);
         }
     }
-    while(list.size() != 0){
-        cout << list.top() << " ";
-        list.pop();
+    if(s == "no" || list.size() != 1){
+        ans = -100000;
     }
+    else ans = list.top();
     return ans;
 }
 
 int main(int argc, const char * argv[]) {
     int a, b;
     vector<double> num(NUMS);
-    string cul = "+++", s = "12+23**";
-    rep(i, NUMS) cin >> num[i];
+    double ans = 0.0;
+    string cul = "+++", s = "8115/-/";
+    //rep(i, NUMS) cin >> num[i];
     
-    ReversePolishNotation(s);
+    ans = ReversePolishNotation(s);
     
-    cout << "a" << endl;
+    cout << ans << endl;
     return 0;
 }
