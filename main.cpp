566aa67f1fda83080f4c8adf9658981260ea64b7
poisonedapple <s18515@salesio-sp.ac.jp>
2021-11-24 22:05:27 +0900

    カッコの実装

--- main.cpp
+++ main.cpp
@@ -77,23 +77,35 @@ int space(string s){
 }
 
 string Tenset(string s){
-    vector<int> poz = {0, 2, 4, 6};
-    string tmp = "";
-    tmp += s[0];
-    s = s.substr(1);
+    stack<string> list;
+    string a = "", b = "", tmp = "";
     while(s != ""){
+        tmp = "(";
         if(isOpe(s[0])){
-            tmp[space(tmp)] = s[0];
+            a = list.top();
+            list.pop();
+            tmp += list.top();
+            list.pop();
+            tmp += s[0];
+            tmp += a;
+            tmp += ")";
+            list.push(tmp);
             s = s.substr(1);
-            // カッコの挿入をこれからやる
         }
         else {
-            tmp += " ";
-            tmp += s[0];
+            list.push(s.substr(0,1));
             s = s.substr(1);
         }
     }
-    return tmp;
+    return list.top();
+}
+
+string Allset(int a0, int a1, int a2, int a3){
+    vector<double> set(4);
+    set[0] = a0, set[1] = a1, set[2] = a2, set[3] = a3;
+    string s = "";
+    
+    return s;
 }
 
 int main(int argc, const char * argv[]) {
@@ -102,6 +114,8 @@ int main(int argc, const char * argv[]) {
     double ans = 0.0;
     string cul = "+++", s = "6234/-+", c = "aaabbbccc";
     //rep(i, NUMS) cin >> num[i];
+    
+    
     
     ans = ReversePolishNotation(s);
     cout << ans << endl;
